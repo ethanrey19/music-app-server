2d555b139c69716730ea59bde571c0bbc659c4c8
Ethan <ebud6805@gmail.com>
2024-01-09 22:22:52 -0800

    initial commit

---
+++ src/main/java/me/ethan/dobbylistensapp/DobbyListensAppApplication.java
@@ -0,0 +1,13 @@
+package me.ethan.dobbylistensapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobbyListensAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobbyListensAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ethan/dobbylistensapp/DobbyListensAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ethan.dobbylistensapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobbyListensAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
